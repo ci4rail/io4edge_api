bcf73acb97b13c205adf163418adb23ed13db23a
KlausPopp <klauspopp@gmx.de>
2023-07-21 18:38:37 +0000

    fix make rules

--- rules.mk
+++ rules.mk
@@ -6,27 +6,30 @@
 # PROTOS: List of all proto files
 
 # search path for files
-VPATH = ./proto/$(MODULE):./go/$(MODULE):./protobuf-c/$(MODULE)
+VPATH ?= ./proto/$(MODULE):./go/$(MODULE):./protobuf-c/$(MODULE):./python/$(MODULE)
 WELLKNOWNTYPES_PROTO = ../google_wellknowntypes/proto
 
 # GOLANG
 %.pb.go ::  %.proto
 	@mkdir -p go/
-	protoc -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --go_out=go/
+	@echo " generating $@"
+	@protoc -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --go_out=go/
 
 GO_TARGETS := $(PROTOS:.proto=.pb.go)
 
 # C
 %.pb-c.c ::  %.proto
 	@mkdir -p protobuf-c/$(MODULE)
-	protoc -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --c_out=protobuf-c/$(MODULE)
+	@echo " generating $@"
+	@protoc -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --c_out=protobuf-c/$(MODULE)
 
 C_TARGETS := $(PROTOS:.proto=.pb-c.c)
 
 # PYTHON
 %_pb2.py ::  %.proto
 	@mkdir -p python/
-	protoc -I./proto -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --python_out=python/
+	@echo " generating $@"
+	@protoc -I./proto -I./proto/$(MODULE) -I$(WELLKNOWNTYPES_PROTO) $< --python_out=python/
 
 PY_TARGETS := $(PROTOS:.proto=_pb2.py)
 
